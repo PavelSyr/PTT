61d89708636a556c5f483856cfb42bcdf24f13a7
Pavel Syrnev <groupozzy@gmail.com>
2023-07-05 23:40:43 +0700

    define ui/mutations name as constants

--- src/app/vstore/ui/vactions.js
+++ src/app/vstore/ui/vactions.js
@@ -1,25 +1,27 @@
+import * as types from './mutation-types';
+
 export const actions = {
     showTaskEditPopup(contex, payload) {
-        contex.commit('SHOW_EDIT_TASK_POPUP', payload);
+        contex.commit(types.SHOW_EDIT_TASK_POPUP, payload);
     },
 
     closeTaskEditPopup(contex) {
-        contex.commit('CLOSE_EDIT_TASK_POPUP');
+        contex.commit(types.CLOSE_EDIT_TASK_POPUP);
     },
     
     showTaskDeletePopup(contex, payload) {
-        contex.commit('SHOW_DELETE_TASK_POPUP', payload);
+        contex.commit(types.SHOW_DELETE_TASK_POPUP, payload);
     },
     
     closeTaskDeletePopup(contex) {
-        contex.commit('CLOSE_DELETE_TASK_POPUP');
+        contex.commit(types.CLOSE_DELETE_TASK_POPUP);
     },
 
     showDeleteAllTasksPopup(context, payload) {
-        context.commit('SHOW_DELETE_ALL_TASKS_POPUP', payload);
+        context.commit(types.SHOW_DELETE_ALL_TASKS_POPUP, payload);
     },
 
     closeDeleteAllTasksPopup(context) {
-        context.commit('CLOSE_DELETE_ALL_TASKS_POPUP');
+        context.commit(types.CLOSE_DELETE_ALL_TASKS_POPUP);
     },
 }
--- src/app/vstore/ui/vmutations.js
+++ src/app/vstore/ui/vmutations.js
@@ -1,30 +1,32 @@
+import * as types from './mutation-types';
+
 export const mutations = {
-    SHOW_EDIT_TASK_POPUP(state, payload) {
+    [types.SHOW_EDIT_TASK_POPUP] (state, payload) {
         state.popups.editTaskPopup.taskData = payload;
         state.popups.editTaskPopup.isActive = true;
     },
 
-    CLOSE_EDIT_TASK_POPUP(state) {
+    [types.CLOSE_EDIT_TASK_POPUP] (state) {
         state.popups.editTaskPopup.isActive = false;
         state.popups.editTaskPopup.taskData = undefined;
     },
 
-    SHOW_DELETE_TASK_POPUP(state, payload) {
+    [types.SHOW_DELETE_TASK_POPUP] (state, payload) {
         state.popups.deleteTaskPopup.taskData = payload;
         state.popups.deleteTaskPopup.isActive = true;
     },
 
-    CLOSE_DELETE_TASK_POPUP(state) {
+    [types.CLOSE_DELETE_TASK_POPUP] (state) {
         state.popups.deleteTaskPopup.isActive = false;
         state.popups.deleteTaskPopup.taskData = undefined;
     },
 
-    SHOW_DELETE_ALL_TASKS_POPUP(state, payload) {
+    [types.SHOW_DELETE_ALL_TASKS_POPUP] (state, payload) {
         state.popups.deleteAllTasksPopup.tasksData = payload;
         state.popups.deleteAllTasksPopup.isActive = true;
     },
 
-    CLOSE_DELETE_ALL_TASKS_POPUP(state) {
+    [types.CLOSE_DELETE_ALL_TASKS_POPUP] (state) {
         state.popups.deleteAllTasksPopup.isActive = false;
         state.popups.deleteAllTasksPopup.tasksData = undefined;
     },
