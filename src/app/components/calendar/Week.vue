5c295135306aaa772794dd8195fdcfd84f5df8d3
Pavel Syrnev <groupozzy@gmail.com>
2023-07-06 23:52:09 +0700

    calendar - add calendar components

---
+++ src/app/components/calendar/Week.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="columns">
+        <day-of-week
+         v-for="data in daysData"
+         :key="data.id"
+         :data="data"
+        >
+        </day-of-week>
+    </div>
+</template>
+
+<script lang="js">
+import DayOfWeek from './DayOfWeek.vue';
+
+export default {
+    name: "Week",
+    
+    components: {
+        "day-of-week": DayOfWeek,
+    }
+
+    props: {
+        daysData: {
+            type: Array,
+            required: false,
+            default: [],
+        },
+    },
+
+    computed: {
+
+    },
+
+    data() {
+        return {
+
+        };
+    },
+
+    methods: {
+
+    }
+
+}
+</script>
